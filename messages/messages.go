c435b7cef39e0dd2abe85fde46fa88aae79ff5b5
Will O'Donovan <wjodon@gmail.com>
2019-02-12 18:14:35 -0500

    Added handling for the rest of the badge dump packet types - no structure yet

--- messages/messages.go
+++ messages/messages.go
@@ -24,6 +24,18 @@ func ProcessPackets(packetsIn chan *irp.Packet) {
 		case C.OPCODE_GAME_ID:
 			gameID := uint16(packet.Payload & 0x0fff)
 			fmt.Println("** Game ID Received:", gameID)
+		case C.OPCODE_BADGE_RECORD_COUNT:
+			hitCount := uint16(packet.Payload & 0x0fff)
+			fmt.Println("** Badge Record Count Received:", hitCount)
+		case C.OPCODE_BADGE_UPLOAD_HIT_RECORD_BADGE_ID:
+			hitBadgeID := uint16(packet.Payload & 0x01ff)
+			fmt.Println("** Badge Upload Hit Record Badge ID Received:", hitBadgeID)
+		case C.OPCODE_BADGE_UPLOAD_HIT_RECORD_TIMESTAMP:
+			hitTimestamp := uint16(packet.Payload & 0x0fff)
+			fmt.Println("** Badge Upload Hit Record Timestamp Received:", hitTimestamp)
+		case C.OPCODE_SET_BADGE_TEAM:
+			hitTeam := uint16(packet.Payload & 0x0fff)
+			fmt.Println("** Badge Upload Hit Record Team Received:", hitTeam)
 		default:
 			fmt.Println("** Opcode", opcode, "not handled yet")
 		}
