d16681b4d64e9a26a3050642eeadf922661f2a23
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-02-18 22:23:51 -0500

    start of gamelist structure

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Games from '../games.json';
 
-const GameList = () => <h3> Game LIst...</h3>
+const GameList = () => {
+  return <div>
+    <h3> Game List...</h3>
+    {Games.map((x, i) => {
+      return <div key={i}>
+        <ul style={{ listStyle: "none" }}>
+          <li> GameID: {x.GameID} </li>
+          <li> StartTime: {x.StartTime} </li>
+          <li> Duration: {x.Duration} </li>
+          <li> Variant: {x.Variant} </li>
+          <li> Team: {x.Team} </li>
+        </ul>
+      </div>
+    })}
+  </div>
+}
 
 export default GameList;
