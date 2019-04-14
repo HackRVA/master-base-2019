a93a177bff92bdd267e2fcb9af24760199662609
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-04-14 17:19:12 -0400

    adding routine to send gameData to Leaderboard

--- baseapi/game.go
+++ baseapi/game.go
@@ -1,10 +1,39 @@
 package baseapi
 
 import (
+	"bytes"
 	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
 
 	bw "github.com/HackRVA/master-base-2019/badgewrangler"
+	"github.com/joho/godotenv"
 )
+
+// PostGameData -- sends gameData to the leaderboard
+func PostGameData(gameData []string) {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error().Msg("Error loading .env file")
+	}
+	uri := os.Getenv("LEADERBOARD_API")
+	json := `{"data":[` + strings.Join(gameData, ",") + `]}`
+	var jsonStr = []byte(json)
+	logger.Info().Msg(json)
+
+	req, err := http.NewRequest("POST", uri+"consume", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Error().Msgf("error connecting to Leaderboard: %s", err)
+	}
+
+	logger.Info().Msg("sending data to leaderboard")
+	defer resp.Body.Close()
+}
 
 // SendGameData - Send GameData to Leaderboard
 func SendGameData(gameDataIn chan *bw.GameData) {
